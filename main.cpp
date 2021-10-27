c81e4dc85e9111d1a659b9834b01ceafbbaf45ab
NexON39 <flashxd30501337@gmail.com>
2021-10-27 18:47:25 +0200

    commit
    commit

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int a,b,tests;
+
+void primegenerator(int one, int two)
+{
+    for(int repeat=one; repeat<=two; repeat++)
+    {
+        if(repeat<2)
+        {
+            repeat = 1;
+        }
+        else
+        {
+            for(int i=2; i<=two; i++)
+            {
+                if(repeat%i==0)
+                {
+                    if(i==repeat)
+                    {
+                        cout << repeat << endl;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+    cin >> tests;
+
+    for(int i=0; i<tests; i++)
+    {
+        cin >> a >> b;
+        primegenerator(a,b);
+        cout << endl;
+    }
+}
