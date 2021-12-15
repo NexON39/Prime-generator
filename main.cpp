bda8843b850b35c0c406200d03c8eaa30087daca
NexON39 <flashxd30501337@gmail.com>
2021-12-15 20:26:56 +0100

    commit

--- main.cpp
+++ main.cpp
@@ -1,29 +1,18 @@
 #include <iostream>
-
 using namespace std;
 
 int a,b,tests;
 
-void primegenerator(int one, int two)
-{
-    for(int repeat=one; repeat<=two; repeat++)
-    {
-        if(repeat<2)
-        {
+void primegenerator(int one, int two) {
+    for(int repeat=one; repeat<=two; repeat++) {
+        if(repeat<2) {
             repeat = 1;
-        }
-        else
-        {
-            for(int i=2; i<=two; i++)
-            {
-                if(repeat%i==0)
-                {
-                    if(i==repeat)
-                    {
-                        cout << repeat << endl;
-                    }
-                    else
-                    {
+        } else {
+            for(int i=2; i<=two; i++) {
+                if(repeat%i==0) {
+                    if(i==repeat) {
+                         cout << repeat << endl;
+                    } else {
                         break;
                     }
                 }
@@ -32,12 +21,10 @@ void primegenerator(int one, int two)
     }
 }
 
-int main()
-{
+int main() {
     cin >> tests;
 
-    for(int i=0; i<tests; i++)
-    {
+    for(int i=0; i<tests; i++) {
         cin >> a >> b;
         primegenerator(a,b);
         cout << endl;
